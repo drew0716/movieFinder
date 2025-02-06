f501617eec532689fdcea3a364a3c0a340b35c12
Drew Galbraith <drew0716@gmail.com>
2025-02-06 14:57:34 -0500

    changed back to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.scss"; // Import global styles only once
 
 const isProd = import.meta.env.MODE === "production";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <HashRouter>
+    <BrowserRouter basename={isProd ? "/movieFinder" : "/"}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
 );
